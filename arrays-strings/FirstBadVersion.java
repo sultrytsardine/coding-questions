d7863c54dcb1f2d8b9705022b6265c08bb97f027
Liam White <liamthomaswhite1@gmail.com>
2020-06-19 16:29:44 +0100

    first bad version

---
+++ arrays-strings/FirstBadVersion.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+/*
+ * Given a bad version number, find the version number that caused the issues. Use the provided API "isBadVersion" (provided by the problem).
+ * https://leetcode.com/problems/first-bad-version
+ */
+public class FirstBadVersion {
+    public int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+        
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        
+        return left;
+    }
+}
