61f28403b49b95ce775f374c62d675e73f060130
Liam White <liamthomaswhite1@gmail.com>
2020-06-26 20:47:35 +0100

    binary right side view

---
+++ trees/BinaryTreeRightSizeView.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+/*
+ * Given the root of a binary tree, return the right-sided view of the tree (i.e. the rightmost node at each level).
+ * This uses a reverse level order traversal and adds the rightmost node at each level to the results.
+ * https://leetcode.com/problems/binary-tree-right-side-view
+ */
+public class BinaryTreeRightSizeView {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        
+        if (root == null) return result;
+        queue.offer(root);
+        
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            for(int i = 0; i < size; i++) {
+                TreeNode current = queue.poll();
+                if (i == 0) result.add(current.val);
+                if (current.right != null) queue.offer(current.right);
+                if (current.left != null) queue.offer(current.left);
+            }
+        }
+        
+        return result;
+    }
+
+    class TreeNode {
+        TreeNode left, right;
+        int val;
+    }
+}
