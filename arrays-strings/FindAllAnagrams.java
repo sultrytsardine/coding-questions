bd6b9b5de9d82c170d817616a2d16fd191a3476f
Liam White <liamthomaswhite1@gmail.com>
2020-07-06 19:41:17 +0100

    find all anagrams

---
+++ arrays-strings/FindAllAnagrams.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+/*
+ * Given a string s and a string p, find all anagrams of p in s and return the start index of each anagram in s.
+ * https://leetcode.com/problems/find-all-anagrams-in-a-string
+ */
+public class FindAllAnagrams {
+    public List<Integer> findAnagrams(String s, String p) {
+        int sLen = s.length(), pLen = p.length();
+        int[] sMap = new int[26];
+        int[] pMap = new int[26];
+        
+        List<Integer> result = new ArrayList<>();
+        
+        for (int i = 0; i < pLen; i++) {
+            char c = p.charAt(i);
+            pMap[c - 'a']++;
+        }
+        
+        for (int i = 0; i < sLen; i++) {
+            char c = s.charAt(i);
+            sMap[c - 'a']++;
+            
+            if (i >= pLen) {
+                c = s.charAt(i - pLen);
+                if (sMap[c - 'a'] > 0) {
+                    sMap[c - 'a']--;
+                } 
+            }
+            
+            
+            if (Arrays.equals(sMap, pMap)) {
+                result.add(i - pLen + 1);
+            }
+        }
+        
+        return result;
+    }
+}
